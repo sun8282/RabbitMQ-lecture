54ffc09bf817bf1d49e432cb527ada9568289d65
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-07-23 11:17:56 +0900

    add: RabbitMQConfig

---
+++ src/main/java/com/example/demo/step0/RabbitMQConfig.java
@@ -0,0 +1,44 @@
+package com.example.demo.step0;
+
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sound.midi.Receiver;
+
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String QUEUE_NAME = "helloqueue";
+
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE_NAME, false);
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        return new RabbitTemplate(connectionFactory);
+    }
+
+    @Bean
+    public SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+                                                    MessageListenerAdapter listenerAdapter) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.addQueueNames(QUEUE_NAME);
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
+
+    @Bean
+    public MessageListenerAdapter listenerAdapter(Receiver receiver) {
+        return new MessageListenerAdapter(Receiver, "receiverMessage");
+    }
+}
