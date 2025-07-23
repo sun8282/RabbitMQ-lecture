f59f6591bcd8ddeba3bccf655fad6df6af3ce0a2
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-07-23 11:31:49 +0900

    add: MessageController

---
+++ src/main/java/com/example/demo/step0/MessageController.java
@@ -0,0 +1,22 @@
+package com.example.demo.step0;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/messages")
+public class MessageController {
+    private final Sender sender;
+
+    public MessageController(Sender sender) {
+        this.sender = sender;
+    }
+
+    @PostMapping("/send")
+    public String sendMessage(@RequestBody String message) {
+        sender.send(message);
+        return "[#] Message send successfully: " + message;
+    }
+}
