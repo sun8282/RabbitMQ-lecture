897f253a9ad83995111a359ba53a98aa5d8cce7c
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-07-23 11:20:52 +0900

    add: Sender

---
+++ src/main/java/com/example/demo/step0/Sender.java
@@ -0,0 +1,19 @@
+package com.example.demo.step0;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Sender {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public Sender(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void send(String message) {
+        rabbitTemplate.convertAndSend(RabbitMQConfig.QUEUE_NAME, message);
+        System.out.println("[#] Sent: " + message);
+    }
+}
